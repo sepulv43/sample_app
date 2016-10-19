1e0d9dcd206595d0a935f31b7a4fa93ec8e224c2
Jennifer Sepulveda <jennifer.sepulveda01>
2016-10-19 10:27:20 -0400

    update again

--- web.py
+++ web.py
@@ -17,5 +17,7 @@ def results():
 	giphyobject = giphypop.Giphy()
 	results = giphyobject.search(searchterm) 
 	return render_template('results.html', results=results)
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+
+if __name__ == "__main__":
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host="0.0.0.0", port=port)
